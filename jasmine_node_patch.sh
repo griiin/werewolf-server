bca43bd588b62145579e2cfb67c820bfbc4d74c1
griiin <julien.nigri@gmail.com>
2014-07-15 00:11:39 +0200

    updating fix script

--- jasmine_node_patch.sh
+++ jasmine_node_patch.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-sed 's/>=0.2.0/0.4.1/g' ./node_modules/grunt-jasmine-node/node_modules/jasmine-node/package.json > ./node_modules/grunt-jasmine-node/node_modules/jasmine-node/package.json;
+
+cd ./node_modules/grunt-jasmine-node/node_modules/jasmine-node;
+echo "changing jasmine-reporter version";
+sed 's/>=0.2.0/0.4.1/g' package.json > tmp;
+mv tmp package.json;
+echo "updating package"
+npm update;
+cd -;
+echo 'done';
