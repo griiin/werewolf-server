66faec6ad69d95a7e716f9918f261a00c2848b8b
griiin <julien.nigri@gmail.com>
2014-08-03 17:23:30 +0200

    Game's city hall should allow user vote => Spec

--- spec/game/cityHallSpec.js
+++ spec/game/cityHallSpec.js
@@ -95,6 +95,30 @@ describe("Game's city hall", function() {
     .done();
   }, this);
 
+  jh.it("should refuse bad vote", function (callback) {
+    var cb = function (data) {
+      var flag = false;
+      data.client.on("vote_response", function (response) {
+        if (!response.result) {
+          flag = true;
+        }
+      });
+      data.client.emit("vote", {target: 'UNKNOWN_username'});
+      data.client.on("cityhall_stop", function (response) {
+        expect(flag).toBe(true);
+        callback();
+      });
+    };
+    var data = {
+      port : this.options.socketport,
+      cityHallCB: cb
+    };
+    game.prepareClassicGame(data)
+    .then(game.goToSecondCityHallAndKillAllWerewolves)
+    .then(game.launchClassicGame)
+    .done();
+  }, this);
+
   //
   // jh.it("should denied user vote", function (callback) {
   //   game.prepareClassicGame({port : this.options.socketport})
